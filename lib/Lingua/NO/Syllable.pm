38b5b2f7ca930bdda36ae85a879df282f942a4b1
Tore Aursand <toreau@gmail.com>
2015-08-30 12:43:45 +0200

    Bumped to version 0.04.

--- lib/Lingua/NO/Syllable.pm
+++ lib/Lingua/NO/Syllable.pm
@@ -21,11 +21,11 @@ Lingua::NO::Syllable - Count the number of syllables in Norwegian words.
 
 =head1 VERSION
 
-Version 0.03.
+Version 0.04.
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
